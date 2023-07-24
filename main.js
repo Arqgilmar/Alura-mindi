c2127c901f9dd1ca0662054136790ac116a1e06d
Gilmar Correa Morais Junior <arquitetogilmarjr@gmail.com>
2023-07-24 17:03:45 -0300

    24/07

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-document.querySelector('tecla_pom');
+function tocaSomPom () {
+    
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+cont listaDeSons = document.querySelector('.sons').play();
+
+for (let i = 0; i < 9; i++) {
+    listaDeTeclas[i].onclick = sons[i];
+}
